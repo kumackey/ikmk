539643e674faa125dcd4696099c786b35e18dfcc
kumackey <kumak1t09e0@gmail.com>
2023-08-09 23:10:25 +0000

    mv

--- Class.hs
+++ Class.hs
@@ -6,3 +6,31 @@ postKey keyName variations :: String -> Bool -> Bool
 -- variation
 -- get /keys
 -- post /keys
+
+
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Network.HTTP.Types (status200)
+
+----main = do
+----    run 8000 app
+----
+----app req respond = respond $
+----    case pathInfo req of
+----        x -> index x
+----
+----
+----
+----index x = responseBuilder status200 [("Content-Type", "application/json")] $ mconcat (map copyByteString [ "hello" ])
+--
+----main :: IO()
+----main = do
+----    let foo = Foo 1 "hello"
+----    (putStrLn . show) foo
+----
+----data Foo = Foo { id :: Int, content :: String } deriving Show
+----
+----instance ToJSON Foo where
+----  toJSON (Foo id content) = object [ "id" .= id,
+----                                     "content" .= content ]
+--
--- main/Main.hs
+++ main/Main.hs
@@ -5,40 +5,11 @@ module Main (main) where
 import FizzBuzz.FizzBuzz as FB
 import DarkLaunch.Store
 
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Network.HTTP.Types (status200)
 import Blaze.ByteString.Builder (copyByteString)
-import qualified Data.ByteString.UTF8 as BU
-import Data.Monoid
-import Control.Applicative
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import qualified Data.Vector as V
 import System.Environment (getArgs)
---
-----main = do
-----    run 8000 app
-----
-----app req respond = respond $
-----    case pathInfo req of
-----        x -> index x
-----
-----
-----
-----index x = responseBuilder status200 [("Content-Type", "application/json")] $ mconcat (map copyByteString [ "hello" ])
---
-----main :: IO()
-----main = do
-----    let foo = Foo 1 "hello"
-----    (putStrLn . show) foo
-----
-----data Foo = Foo { id :: Int, content :: String } deriving Show
-----
-----instance ToJSON Foo where
-----  toJSON (Foo id content) = object [ "id" .= id,
-----                                     "content" .= content ]
---
 
 instance FromNamedRecord KeyRecord where
     parseNamedRecord r = KeyRecord <$> r .: "name" <*> r .: "variations"
@@ -46,7 +17,7 @@ instance FromNamedRecord KeyRecord where
 subCommand :: String -> IO ()
 subCommand a
         | a == "darklaunch" = do
-            csvData <- BL.readFile "keys.csv"
+            csvData <- BL.readFile "src/DarkLaunch/keys.csv"
             case decodeByName csvData of
                 Left err -> putStrLn err
                 Right (_, v) -> V.forM_ v $ \ k -> do
