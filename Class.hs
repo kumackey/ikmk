613f618fb616d548cc75bf9b03143c3805375bc8
kumackey <kumak1t09e0@gmail.com>
2023-08-09 11:38:24 +0000

    fizzbuzz

--- Class.hs
+++ Class.hs
@@ -1,14 +1,8 @@
-class Foo a where
-    foo :: a -> String
-instance Foo Bool where
-    foo True = "Bool: True"
-    foo False = "Bool: False"
-instance Foo Int where
-    foo x = "Int: " ++ show x
-instance Foo Char where
-    foo x = "Char: " ++ [x]
 
-main = do
-    putStrLn $ foo True		-- Bool: True
-    putStrLn $ foo (123::Int)	-- Int: 123
-    putStrLn $ foo 'A'		-- Char: A
+
+postKey keyName variations :: String -> Bool -> Bool
+
+
+-- variation
+-- get /keys
+-- post /keys
---
+++ main/Main.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import FizzBuzz.FizzBuzz
+
+main :: IO ()
+main = loop 100 $ (putStrLn . fizzbuzz)
+
+--import Network.Wai
+--import Network.Wai.Handler.Warp
+--import Network.HTTP.Types (status200)
+--import Blaze.ByteString.Builder (copyByteString)
+--import qualified Data.ByteString.UTF8 as BU
+--import Data.Monoid
+--import Data.Aeson
+--import Control.Applicative
+--import qualified Data.ByteString.Lazy as BL
+--import Data.Csv
+--import qualified Data.Vector as V
+--import System.Environment (getArgs)
+--
+----main = do
+----    run 8000 app
+----
+----app req respond = respond $
+----    case pathInfo req of
+----        x -> index x
+----
+----
+----
+----index x = responseBuilder status200 [("Content-Type", "application/json")] $ mconcat (map copyByteString [ "hello" ])
+--
+----main :: IO()
+----main = do
+----    let foo = Foo 1 "hello"
+----    (putStrLn . show) foo
+----
+----data Foo = Foo { id :: Int, content :: String } deriving Show
+----
+----instance ToJSON Foo where
+----  toJSON (Foo id content) = object [ "id" .= id,
+----                                     "content" .= content ]
+--
+--convStringToBool :: String -> Maybe Bool
+--convStringToBool str
+--                   | str == "True" = Just True
+--                   | str == "False" = Just False
+--                   | otherwise = Nothing
+--
+--convKeyRecordToKey :: KeyRecord -> Maybe Key
+--convKeyRecordToKey record = do
+--    let rec = (convStringToBool .recordVariations) record
+--    case rec of
+--        Just r -> Just (Key (recordName record) r)
+--        Nothing -> Nothing
+--
+--data Key = Key
+--    { name   :: String
+--    , variations :: Bool
+--    }
+--
+--data KeyRecord = KeyRecord
+--                     { recordName   :: !String
+--                     , recordVariations :: !String
+--                     }
+--instance FromNamedRecord KeyRecord where
+--    parseNamedRecord r = KeyRecord <$> r .: "name" <*> r .: "variations"
+--
+--subCommand :: String -> IO ()
+--subCommand a
+--        | a == "darklaunch" = do
+--            csvData <- BL.readFile "keys.csv"
+--            case decodeByName csvData of
+--                Left err -> putStrLn err
+--                Right (_, v) -> V.forM_ v $ \ k -> do
+--                    let km = convKeyRecordToKey(k)
+--                    case km of
+--                        Just km -> putStrLn $ "Key Name: " ++ name km ++ ", Variations: " ++ show (variations km)
+--                        Nothing -> putStrLn "invalid"
+--        | a == "fizzbuzz" = putStrLn "fizzbuzz"
+--        | otherwise = putStrLn "Unknown application"
+--
+--main :: IO ()
+--main = do
+--    args <- getArgs
+--    if 1 <= length args
+--      then subCommand (args !! 0)
+--      else putStrLn "Arguments are too short"
